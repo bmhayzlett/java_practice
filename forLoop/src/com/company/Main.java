74fab44b35ca7971b2c4f3c8d070511cce16dde4
Brian Hayzlett <brianhayzlett@yahoo.com>
2016-10-26 18:49:01 -0700

    for loop

--- forLoop/src/com/company/Main.java
+++ forLoop/src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        double test;
+
+        for(int i=8; i > 1; i--) {
+            test = calculateInterest(10000.0, (double) i);
+            System.out.println("10000 at " + i + "% interest is " + String.format("%.2f",test));
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return amount * (interestRate / 100);
     }
 }
